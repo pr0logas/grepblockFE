c014fc8c868cbdd8521a8dcba78cf2e3832f59cc
Mister Nemo <miister.nemo@gmail.com>
2019-09-18 17:48:37 +0300

    Sidenav affix on page update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -128,3 +128,16 @@ jQuery(document).ready(function($) {
   }
 
 });
+
+$('body').on( "update_page", function() {
+
+  var $window = $(window);
+
+  $('.bs-docs-sidenav').affix({
+    offset: {
+      top: function () { return $window.width() <= 980 ? 290 : 210 }
+    , bottom: 270
+    }
+  });
+
+});
