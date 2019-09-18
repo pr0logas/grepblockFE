76bf170933ebade7b1576651385083bd4c18509e
Mister Nemo <miister.nemo@gmail.com>
2019-09-18 17:49:29 +0300

    New func grep param

--- assets/js/nemo_head.js
+++ assets/js/nemo_head.js
@@ -176,3 +176,12 @@
       var text = document.createTextNode(text);
       el.appendChild(text);
     }
+    function getUrlParamByName(name, url) {
+      if (!url) url = window.location.href;
+      name = name.replace(/[\[\]]/g, '\\$&');
+      var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+          results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
