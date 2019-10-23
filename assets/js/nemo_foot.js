bb7c74148c2db6b35feb3c9dc19c006c91c04e4f
Mister Nemo <miister.nemo@gmail.com>
2019-10-23 18:09:30 +0300

    Fix url hash tag

--- assets/js/nemo_foot.js
+++ assets/js/nemo_foot.js
@@ -56,7 +56,7 @@
     function show_page(el) {
       if(!el) return
 
-      window.history.pushState('page2', 'Title', '/?page='+el.id.replace('link-','')+'&coin='+getUrlParamByName('coin')+'&type='+getUrlParamByName('type')+'&query='+getUrlParamByName('query')+'#'+url());
+      window.history.pushState('page2', 'Title', '/?page='+el.id.replace('link-','')+'&coin='+getUrlParamByName('coin')+'&type='+getUrlParamByName('type')+'&query='+getUrlParamByName('query')+url());
 
       menu_deactive();
 
