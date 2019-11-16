27b3e080b2a2d666090a451bac2fae3754ca8cd2
pr0logas <prologas@protonmail.com>
2019-11-16 16:09:42 +0200

    Test global GB value 2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -156,9 +156,9 @@ jQuery(document).ready(function($) {
       case 'GlobalBlockChainSize':
         var chainsize_formated = (parseFloat(value) / 1073741824)
         console.log(chainsize_formated)
-        var chainsize_formated2 = parseFloat(chainsize_formated).toFixed(1).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-        chainsize_formated2 = chainsize_formated2.substring(0, wallets_formated.length-2);
-        return chainsize_formated2;
+        //var chainsize_formated2 = parseFloat(chainsize_formated).toFixed(1).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+        //chainsize_formated2 = chainsize_formated2.substring(0, wallets_formated.length-2);
+        return chainsize_formated;
         break;
       default:
         return value;
